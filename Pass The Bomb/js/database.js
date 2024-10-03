9791ed9bba668bbaae1f895036659f24ff04bae7
Chiispiitas <66695292+Chiispiitas@users.noreply.github.com>
2024-10-03 18:32:40 -0500

    B2 Certificate

---
+++ Pass The Bomb/js/database.js
@@ -0,0 +1,40 @@
+"use strict";
+/* ==============================================
+     Database
+     Made by: David Santana
+============================================== */
+
+var dictionary = defaultDictionary;
+var letters = defaultLetters;
+var participantsHash = {};
+var participantsArray = [];
+var participantsCounter = 0;
+var usedWords = [];
+var usedLetters = [];
+
+/* ==============================================
+     Participant Class
+============================================== */
+function Participant(name) {
+     this.name = name;
+     this.lives = 1; // change this
+}
+/* ==============================================
+     Participant Hash to Array
+============================================== */
+function participantHashToArray() {
+     for (let key in participantsHash) {
+          participantsArray.push(new Participant(participantsHash[key]));
+     }
+}
+/* ==============================================
+     Count Element In Array
+============================================== */
+function countArray(array, element) {
+     var count = 0;
+     for(var i = 0; i < array.length; ++i){
+         if(array[i] == element)
+             count++;
+     }
+     return count;
+}
---
+++ Pass The Bomb/js/scenes/scene-participants.js
@@ -0,0 +1,63 @@
+"use strict";
+/* ==============================================
+     Scene Participants
+     Made by: David Santana
+============================================== */
+
+var listParticipants = document.getElementById("list-participants");
+var inputParticipant = document.getElementById("input-participants");
+
+/* ==============================================
+     Add Button Listeners
+============================================== */
+document.getElementById("btn-participants-back").addEventListener( "click", function(){ 
+     sceneLoad("scene-title");
+});
+document.getElementById("btn-participants-next").addEventListener( "click", function(){ 
+     if (Object.keys(participantsHash).length >= 2) {
+          participantHashToArray();
+          sceneLoad("scene-countdown");
+          startCountdown();
+     } 
+});
+/* ==============================================
+     Add Input Listener
+============================================== */
+inputParticipant.addEventListener("keydown", function(event) {
+     if (event.code === "Enter") {
+          if (participantsCounter < 1000) { addParticipant(inputParticipant.value.trim()); }
+     }
+});
+/* ==============================================
+     Add Participant
+============================================== */
+function addParticipant(name) {
+     inputParticipant.value = "";
+     if (name == "") { return; }
+     listParticipants.innerHTML += generateParticipantAnchor(name);
+     participantsCounter += 1;
+     // if (participantsCounter == 10) { inputParticipant.disabled = true; }
+}
+/* ==============================================
+     Generate Participant Anchor
+============================================== */
+function generateParticipantAnchor(name) {
+     let id = `${name}-${participantsCounter}-${Math.floor(Math.random() * 999999) + 1}`;
+     participantsHash[id] = name;
+     return `<a id="${id}" onClick="removeParticipant('${id}');">${name}</a>`;
+}
+/* ==============================================
+     Remove Participant
+============================================== */
+function removeParticipant(id) {
+     participantsCounter -= 1;
+     delete participantsHash[id];
+     inputParticipant.disabled = false;
+     document.getElementById(id).outerHTML = "";
+}
+/* ==============================================
+     Update Participants
+============================================== */
+var updateParticipants = setInterval(function() {
+     if (sceneCurrent == "scene-participants") { inputParticipant.focus(); }
+}, 1);
