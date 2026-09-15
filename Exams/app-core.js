b764cf7af2bf01f3cd2c1d54ad52aa513adcf9aa
Chiispiitas <66695292+Chiispiitas@users.noreply.github.com>
2026-09-14 19:52:56 -0500

    Advance Brighton answer-key release to 2026-09-14.1

--- Exams/app-core.js
+++ Exams/app-core.js
@@ -193,7 +193,7 @@
    Immutable answer-key release bridge
    ---------------------------------------------- */
 (() => {
-  const ANSWER_KEY_VERSION = "2026-09-08.1";
+  const ANSWER_KEY_VERSION = "2026-09-14.1";
   const VERSION_SEPARATOR = "@@";
   const VERSIONED_POST_ENDPOINT = /\/(?:submitExam|submitTest|updateProgress)(?:\?|$)/i;
   const VERSIONED_RESULT_ENDPOINT = /\/(?:getResults|getTestResults|getProgress)(?:\?|$)/i;
