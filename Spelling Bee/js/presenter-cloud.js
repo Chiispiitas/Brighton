dff82d928c563ce848762e2f87eb823d302b62d2
Chiispiitas <66695292+Chiispiitas@users.noreply.github.com>
2026-09-18 09:20:06 -0500

    Reduce Admin to Presenter judgement latency

--- Spelling Bee/js/presenter-cloud.js
+++ Spelling Bee/js/presenter-cloud.js
@@ -359,9 +359,9 @@
   // without waiting for the periodic state scan.
   window.requestSpellingPresenterSync = () => publishPresenter(false);
 
-  window.setInterval(() => publishPresenter(false), 300);
+  window.setInterval(() => publishPresenter(false), 200);
   window.setInterval(() => publishPresenter(true), 3000);
-  window.setInterval(pollRemoteCommands, 180);
+  window.setInterval(pollRemoteCommands, 90);
 
   updateViewLinks();
   const saved = Cloud.normalizeSessionCode(localStorage.getItem("brighton-spelling-presenter-session"));
